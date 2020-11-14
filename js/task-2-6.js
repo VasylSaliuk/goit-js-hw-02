963625063bc738ad804df3da13cff1ebb0fbeae7
Vasyl Saliuk <67233665+VasylSaliuk@users.noreply.github.com>
2020-11-14 18:25:18 +0200

    finish

--- js/task-2-6.js
+++ js/task-2-6.js
@@ -1,7 +1,7 @@
 function mapArray(array) {
     const numbers = new Array(array.length);
     for(let i = 0; i < array.length; i += 1) {
-      
+      numbers[i] = array[i]*10;
     }
     return numbers;
   }
